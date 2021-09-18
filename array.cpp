3383096c6cca18ff2250a95a4fdf2ad46f65d9e2
Lakshmivarma8 <88922653+Lakshmivarma8@users.noreply.github.com>
2021-09-18 19:26:50 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10],i;
+    cout<<"Enter Elements :\n";
+    for(i=0;i<5;i++)
+    cin>>arr[i];
+    cout<<"You Entered :\n";
+    for(i=0;i<5;i++)
+    cout<<arr[i]<<endl;
+    return 0;
+}
