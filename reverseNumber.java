06e31830bc3f9f6ca8f8da30db6475f5e07af176
Lakshmivarma8 <88922653+Lakshmivarma8@users.noreply.github.com>
2021-10-17 07:26:54 +0530

    Create reverseNumber.java

---
+++ reverseNumber.java
@@ -0,0 +1,16 @@
+public class reverseNumber {
+    public static void main(String[] args)   {  
+        int reverse = 0;  
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ente a number : ");
+        int number=sc.nextInt();
+        while(number != 0)   
+        {  
+            int remainder = number % 10;  
+            reverse = reverse * 10 + remainder;  
+            number = number/10;  
+        }  
+        System.out.println("The reverse of the given number is: " + reverse);  
+}  
+    
+}
