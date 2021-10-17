b3f361aa533e47db72746cbc8f77ebe3a6633310
Lakshmivarma8 <88922653+Lakshmivarma8@users.noreply.github.com>
2021-10-17 07:40:41 +0530

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,12 @@
+public class reverseString {
+    public static void main(String args[]) {
+    String initial, rev="";
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter the string : ");
+    initial=in.nextLine();
+    int length=initial.length();
+    for(int i=length-1;i>=0;i--)
+        rev=rev+initial.charAt(i);
+    System.out.println("Reversed string: "+rev);
+   }
+}
